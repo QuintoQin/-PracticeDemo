2945e5c36804cda394d22bc0c9cb7ee2001f54e9
qinq <qinq@163.com>
2018-01-16 09:03:52 +0800

    2017-1-16

--- view/src/main/java/com/example/view/MainActivity.java
+++ view/src/main/java/com/example/view/MainActivity.java
@@ -11,50 +11,11 @@ import android.widget.Button;
 import com.example.view.simple.MyView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "View";
-    private MyView myView;
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        myView = (MyView) findViewById(R.id.myView);
-//        btn = (Button) findViewById(R.id.btn);
 
-        //mView.smoothScrollTo();
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myView.beginScroll();
-            }
-        });
-        //Log.e(TAG, "slop:" + ViewConfiguration.get(this).getScaledTouchSlop());
     }
-
-    //    @Override
-//    public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//        Log.e(TAG, "Top:" + mView.getTop());
-//        Log.e(TAG, "Left:" + mView.getLeft());
-//        Log.e(TAG, "Right:" + mView.getRight());
-//        Log.e(TAG, "Bottom:" + mView.getBottom());
-//        Log.e(TAG, "x:" + mView.getX());
-//        Log.e(TAG, "y:" + mView.getY());
-//        Log.e(TAG, "translationX:" + mView.getTranslationX());
-//        Log.e(TAG, "translationY:" + mView.getTranslationY());
-//    }
-//
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.e("ACTIVITY TAG", "点击");
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        return super.dispatchTouchEvent(ev);
-    }
-
-
 }
